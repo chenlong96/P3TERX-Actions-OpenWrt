03c0dcb5164ba24992677c1b8d2a82e944dc8c5d
CLy <2063576450@qq.com>
2023-10-19 11:15:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,10 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git'                  >>feeds.conf.default
-echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git     '                 >>feeds.conf.default
-echo 'src-git argonnew https://github.com/jerrykuku/luci-theme-argon.git     '                      >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages'                                    >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small'                                               >>feeds.conf.default
